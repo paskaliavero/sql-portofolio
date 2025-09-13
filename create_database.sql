f68866430af40f854f4debbee2a362c4fda28a81
Veronika <paskaliavero@gmail.com>
2025-09-13 14:06:08 +0700

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,14 @@
+/* 
+    Create Tables :
+    - outlet
+    - employee
+    - supplier
+    - category
+    - product
+    - transaction
+    - transaction_detail
+*/
+
 CREATE TABLE outlet(
     id_outlet SERIAl PRIMARY KEY,
     outlet_name VARCHAR(100),
@@ -44,3 +55,17 @@ CREATE TABLE transaction_detail (
     quantity INT,
     subtotal INT
 );
+
+
+-- Insert some data into the tables --
+INSERT INTO outlet (outlet_name, address, region, phone) VALUES 
+    ('Halte Balai Kota', 'Gambir, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta', 'Jakarta Pusat','085288984561'),
+    ('Halte Bundaran HI', 'Gondangdia, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta', 'Jakarta Pusat', '08794621354'),
+    ('Halte Bunderan Senayan', 'Halte Busway Bundaran Senayan, RT.1/RW.3, Senayan, Kec. Kby. Baru, Kota Jakarta Selatan', 'Jakarta Selatan', '089764521346'),
+    ('Halte Cawang Central', 'Halte Bus Transjakarta Cawang Sentral, Jl. Mayjen Sutoyo No.5, RW.11, Cawang, Kec. Kramat jati, Kota Jakarta Timur', 'Jakarta Timur', '086457951324'),
+    ('Halte CSW', 'Jl. Trunojoyo No.1, Melawai, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta', 'Jakarta Selatan', '087964523146'),
+    ('Halte Dukuh Atas', 'Dukuh Atas, Menteng, Kecamatan Setiabudi, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta', 'Jakarta Pusat', '085246791346'),
+    ('Halte Juanda', 'Jl. Ir H. Juanda I B No.2 7, RT.13/RW.1, Kb. Klp., Kecamatan Gambir, Kota Jakarta Pusat', 'Jakarta Pusat', '085246791526'),
+    ('Halte Kampung Melayu', 'Bali Mester, Kecamatan Jatinegara, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta', 'Jakarta Pusat', '087946521346'),
+    ('Halte Karet Sudirman', 'Jl. Jend. Sudirman No.Kav 25, Kota SelatanSetia Budi, Karet, Kecamatan, Setiabudi, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta', 'Jakarta Pusat', '087964521346'),
+    ('Halte Monas', 'Gambir, Kecamatan Gambir, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta', 'Jakarta Pusat', '081346798546');
